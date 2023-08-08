541d8defc81747b715c5cca336883f0cb5df959d
Brian Worsham <brian.w.worsham@hotmail.com>
2023-08-08 15:51:23 -0500

    Add project files.

---
+++ Phase3Section2.36/Views/Home/CustomHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace Phase3Section3._36.Views.Home
+{
+    public static class CustomHelper
+    {
+        public static IHtmlContent File(this IHtmlHelper htmlHelper, string id)
+        {
+            var tb = new TagBuilder("input");
+            tb.Attributes.Add("type", "file");
+            tb.Attributes.Add("id", id);
+
+            return tb;
+        }
+    }
+}
